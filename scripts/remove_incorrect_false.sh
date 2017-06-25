addd122de351554cdbbbe6670ec2b0b9a870b368
Benedikt Schemmer <ben@besd.de>
2017-06-25 10:39:54 +0200

    change to not consider cases of == 1 because it can have special meaning

--- scripts/remove_incorrect_false.sh
+++ scripts/remove_incorrect_false.sh
@@ -1,11 +1,11 @@
 #false
 find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#([[:space:]]*\([0-9a-zA-Z_]*\)[[:space:]]*!=[[:space:]]*true[[:space:]]*)#(!\1)#i' {} \;
-find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#([[:space:]]*\([0-9a-zA-Z_]*\)[[:space:]]*!=[[:space:]]*1[[:space:]]*)#(!\1)#i' {} \;
 find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#([[:space:]]*\([0-9a-zA-Z_]*\)[[:space:]]*==[[:space:]]*false[[:space:]]*)#(!\1)#i' {} \;
 find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#([[:space:]]*\([0-9a-zA-Z_]*\)[[:space:]]*==[[:space:]]*0[[:space:]]*)#(!\1)#i' {} \;
+find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#([[:space:]]*\([0-9a-zA-Z_]*\)[[:space:]]*==[[:space:]]*NULL[[:space:]]*)#(!\1)#i' {} \;
+find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#([[:space:]]*\([0-9a-zA-Z_]*\)[[:space:]]*==[[:space:]]*nullptr[[:space:]]*)#(!\1)#i' {} \;
 #true
 find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#([[:space:]]*\([0-9a-zA-Z_]*\)[[:space:]]*==[[:space:]]*true[[:space:]]*)#(\1)#i' {} \;
 find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#([[:space:]]*\([0-9a-zA-Z_]*\)[[:space:]]*!=[[:space:]]*false[[:space:]]*)#(\1)#i' {} \;
-find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#([[:space:]]*\([0-9a-zA-Z_]*\)[[:space:]]*==[[:space:]]*1[[:space:]]*)#(\1)#i' {} \;
 find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#([[:space:]]*\([0-9a-zA-Z_]*\)[[:space:]]*!=[[:space:]]*0[[:space:]]*)#(\1)#i' {} \;
 
