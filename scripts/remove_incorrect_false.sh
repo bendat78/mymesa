ba776853c4bc67530db9e6e676439fe4f1b34ed3
Benedikt Schemmer <ben@besd.de>
2017-06-25 10:45:09 +0200

    consider more true cases

--- scripts/remove_incorrect_false.sh
+++ scripts/remove_incorrect_false.sh
@@ -8,4 +8,5 @@ find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#([[:space:]]*\
 find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#([[:space:]]*\([0-9a-zA-Z_]*\)[[:space:]]*==[[:space:]]*true[[:space:]]*)#(\1)#i' {} \;
 find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#([[:space:]]*\([0-9a-zA-Z_]*\)[[:space:]]*!=[[:space:]]*false[[:space:]]*)#(\1)#i' {} \;
 find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#([[:space:]]*\([0-9a-zA-Z_]*\)[[:space:]]*!=[[:space:]]*0[[:space:]]*)#(\1)#i' {} \;
-
+find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#([[:space:]]*\([0-9a-zA-Z_]*\)[[:space:]]*!=[[:space:]]*NULL[[:space:]]*)#(\1)#i' {} \;
+find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#([[:space:]]*\([0-9a-zA-Z_]*\)[[:space:]]*!=[[:space:]]*nullptr[[:space:]]*)#(\1)#i' {} \;
