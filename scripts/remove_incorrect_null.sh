71bfa6ea519d7253304b108322a3e208622e2c4e
Benedikt Schemmer <ben@besd.de>
2017-06-26 19:15:00 +0200

    slight changes to scripts

--- scripts/remove_incorrect_null.sh
+++ scripts/remove_incorrect_null.sh
@@ -1,5 +1,5 @@
 
-find -not -path '*/\.*' -type f -exec sed -i 's#[[:space:]]*=[[:space:]]*{[[:space:]]*{[[:space:]]*NULL[[:space:]]*}[[:space:]]*,[[:space:]]*{[[:space:]]*NULL[[:space:]]*}[[:space:]]*,[[:space:]]*{[[:space:]]*NULL[[:space:]]*}[[:space:]]*}# = \{\}#' {} \;
-find -not -path '*/\.*' -type f -exec sed -i 's#[[:space:]]*=[[:space:]]*{[[:space:]]*{[[:space:]]*{[[:space:]]*NULL[[:space:]]*}[[:space:]]*}[[:space:]]*}# = \{\}#' {} \;
-find -not -path '*/\.*' -type f -exec sed -i 's#[[:space:]]*=[[:space:]]*{[[:space:]]*{[[:space:]]*NULL[[:space:]]*}[[:space:]]*}# = \{\}#' {} \;
-find -not -path '*/\.*' -type f -exec sed -i 's#[[:space:]]*=[[:space:]]*{[[:space:]]*NULL[[:space:]]*}# = \{\}#' {} \;
+find -not -path '*/\.*' -type f -exec sed -i 's#[[:space:]]*=[[:space:]]*{[[:space:]]*{[[:space:]]*NULL[[:space:]]*}[[:space:]]*,[[:space:]]*{[[:space:]]*NULL[[:space:]]*}[[:space:]]*,[[:space:]]*{[[:space:]]*NULL[[:space:]]*}[[:space:]]*}# = \{\}#ig' {} \;
+find -not -path '*/\.*' -type f -exec sed -i 's#[[:space:]]*=[[:space:]]*{[[:space:]]*{[[:space:]]*{[[:space:]]*NULL[[:space:]]*}[[:space:]]*}[[:space:]]*}# = \{\}#ig' {} \;
+find -not -path '*/\.*' -type f -exec sed -i 's#[[:space:]]*=[[:space:]]*{[[:space:]]*{[[:space:]]*NULL[[:space:]]*}[[:space:]]*}# = \{\}#ig' {} \;
+find -not -path '*/\.*' -type f -exec sed -i 's#[[:space:]]*=[[:space:]]*{[[:space:]]*NULL[[:space:]]*}# = \{\}#ig' {} \;
