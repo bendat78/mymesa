eed00d5e96a51ec89606c2ab7c0bfbe2fd2bf3bb
Benedikt Schemmer <ben@besd.de>
2017-06-26 19:15:00 +0200

    slight changes to scripts

--- scripts/remove_incorrect_init.sh
+++ scripts/remove_incorrect_init.sh
@@ -1,5 +1,5 @@
 
-find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#[[:space:]]*=[[:space:]]*{[[:space:]]*{[[:space:]]*0[[:space:]]*}[[:space:]]*,[[:space:]]*{[[:space:]]*0[[:space:]]*}[[:space:]]*,[[:space:]]*{[[:space:]]*0[[:space:]]*}[[:space:]]*}# = \{\}#' {} \;
-find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#[[:space:]]*=[[:space:]]*{[[:space:]]*{[[:space:]]*{[[:space:]]*0[[:space:]]*}[[:space:]]*}[[:space:]]*}# = \{\}#' {} \;
-find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#[[:space:]]*=[[:space:]]*{[[:space:]]*{[[:space:]]*0[[:space:]]*}[[:space:]]*}# = \{\}#' {} \;
-find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#[[:space:]]*=[[:space:]]*{[[:space:]]*0[[:space:]]*}# = \{\}#' {} \;
+find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#[[:space:]]*=[[:space:]]*{[[:space:]]*{[[:space:]]*0[[:space:]]*}[[:space:]]*,[[:space:]]*{[[:space:]]*0[[:space:]]*}[[:space:]]*,[[:space:]]*{[[:space:]]*0[[:space:]]*}[[:space:]]*}# = \{\}#ig' {} \;
+find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#[[:space:]]*=[[:space:]]*{[[:space:]]*{[[:space:]]*{[[:space:]]*0[[:space:]]*}[[:space:]]*}[[:space:]]*}# = \{\}#ig' {} \;
+find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#[[:space:]]*=[[:space:]]*{[[:space:]]*{[[:space:]]*0[[:space:]]*}[[:space:]]*}# = \{\}#ig' {} \;
+find -not -path '*/\.*' -not -name "*.py" -type f -exec sed -i 's#[[:space:]]*=[[:space:]]*{[[:space:]]*0[[:space:]]*}# = \{\}#ig' {} \;
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,5 @@
 git checkout -- VERSION
 git status
 git pull origin master
-git push -u mymesa mymesa2
+git push -u mymesa master
 git status
