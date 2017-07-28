9617845bff701dfa3eaab1b2780bbc6d76f39994
Marek Olšák <marek.olsak@amd.com>
2017-07-28 23:05:38 +0200

    ac/surface: compute tile swizzle only when it's allowed
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com>

--- src/amd/common/ac_surface.c
+++ src/amd/common/ac_surface.c
@@ -698,10 +698,12 @@ static int gfx6_compute_surface(ADDR_HANDLE addrlib,
 
 	surf->is_linear = surf->u.legacy.level[0].mode == RADEON_SURF_MODE_LINEAR_ALIGNED;
 
-	/* workout base swizzle */
-	if (!(surf->flags & RADEON_SURF_Z_OR_SBUFFER)) {
-		ADDR_COMPUTE_BASE_SWIZZLE_INPUT AddrBaseSwizzleIn = {};
-		ADDR_COMPUTE_BASE_SWIZZLE_OUTPUT AddrBaseSwizzleOut = {};
+	/* Work out tile swizzle. */
+	if (surf->u.legacy.level[0].mode == RADEON_SURF_MODE_2D &&
+	    !(surf->flags & (RADEON_SURF_Z_OR_SBUFFER | RADEON_SURF_SHAREABLE)) &&
+	    (config->info.samples > 1 || !(surf->flags & RADEON_SURF_SCANOUT))) {
+		ADDR_COMPUTE_BASE_SWIZZLE_INPUT AddrBaseSwizzleIn = {0};
+		ADDR_COMPUTE_BASE_SWIZZLE_OUTPUT AddrBaseSwizzleOut = {0};
 
 		AddrBaseSwizzleIn.surfIndex = config->info.surf_index;
 		AddrBaseSwizzleIn.tileIndex = AddrSurfInfoIn.tileIndex;
