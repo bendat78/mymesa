8b374727d6cc9f7b2977188ad635b41d3da83216
Timothy Arceri <tarceri@itsqueeze.com>
2017-07-31 22:48:27 +1000

    mesa/st: fix conditional jump depends on uninitialised value
    Reported by valgrind at:
    glsl_to_tgsi_visitor::visit(ir_expression*) (st_glsl_to_tgsi.cpp:1560)
    
    When compiling the Deus Ex shaders.
    
    Fixes: 28a5e7104 ("st/glsl_to_tgsi: handle precise modifier")
    Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com>
    Reviewed-by: Karol Herbst <karolherbst@gmail.com>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>

--- src/mesa/state_tracker/st_glsl_to_tgsi.cpp
+++ src/mesa/state_tracker/st_glsl_to_tgsi.cpp
@@ -4614,6 +4614,7 @@ glsl_to_tgsi_visitor::glsl_to_tgsi_visitor()
    mem_ctx = ralloc_context(NULL);
    ctx = NULL;
    prog = NULL;
+   precise = 0;
    shader_program = NULL;
    shader = NULL;
    options = NULL;
