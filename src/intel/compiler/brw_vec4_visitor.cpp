4462c600636d6c888981deb90652a782b8d39424
Jason Ekstrand <jason.ekstrand@intel.com>
2017-11-06 17:01:56 -0800

    intel/compiler: Move the destructor from vec4_visitor to backend_shader
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/intel/compiler/brw_vec4_visitor.cpp
+++ src/intel/compiler/brw_vec4_visitor.cpp
@@ -1884,10 +1884,6 @@ vec4_visitor::vec4_visitor(const struct brw_compiler *compiler,
    this->uniforms = 0;
 }
 
-vec4_visitor::~vec4_visitor()
-{
-}
-
 
 void
 vec4_visitor::fail(const char *format, ...)
