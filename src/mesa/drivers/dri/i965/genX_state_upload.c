a3a0ef344f3b9fe0a58b2d0be75243f93be65674
Kenneth Graunke <kenneth@whitecape.org>
2017-11-15 22:40:16 -0800

    i965: Upload invariant state once at the start of the batch on Gen4-5.
    We want to emit invariant state at the start of a render batch.  In the
    past, this more or less happened: a new batch flagged BRW_NEW_CONTEXT
    (because we don't have hardware contexts), which triggered the
    brw_invariant_state atom.  So, it would be emitted before any 3D
    drawing.  (Technically, there might be some BLT commands in the batch
    because Gen4-5 have a single combined render/BLT ring, but that should
    be harmless).
    
    With the advent of BLORP, this broke.  The first item in a batch might
    be a BLORP operation, which bypasses the normal draw upload path.  So,
    we need to ensure invariant state happens first.  To do that, we just
    upload it when creating a new batch.  On Gen6+ we'd need to worry about
    whether it's a RENDER or BLT batch, but because we have a combined ring,
    this approach should work fine on Gen4-5.
    
    Seems to fix GPU hangs when playing hardware accelerated video with
    mpv -hwdec=vaapi on Ironlake.
    
    Cc: mesa-stable@lists.freedesktop.org
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=103529
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

--- src/mesa/drivers/dri/i965/genX_state_upload.c
+++ src/mesa/drivers/dri/i965/genX_state_upload.c
@@ -5372,8 +5372,6 @@ genX(init_atoms)(struct brw_context *brw)
 
       /* Command packets:
        */
-      &brw_invariant_state,
-
       &brw_binding_table_pointers,
       &genX(blend_constant_color),
 
