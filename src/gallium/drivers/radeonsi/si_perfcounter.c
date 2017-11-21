0b9bbef45633f8cfc9e25677506b71d41c55bac6
Marek Olšák <marek.olsak@amd.com>
2017-11-21 01:47:30 +0100

    radeonsi: expose all CB performance counters on Stoney
    Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com>

--- src/gallium/drivers/radeonsi/si_perfcounter.c
+++ src/gallium/drivers/radeonsi/si_perfcounter.c
@@ -373,8 +373,8 @@ static struct si_pc_block groups_CIK[] = {
 };
 
 static struct si_pc_block groups_VI[] = {
-	{ &cik_CB, 396, 4 },
-	{ &cik_CPF, 19, 0 },
+	{ &cik_CB, 405, 4 },
+	{ &cik_CPF, 19 },
 	{ &cik_DB, 257, 4 },
 	{ &cik_GRBM, 34, 0 },
 	{ &cik_GRBMSE, 15, 0 },
