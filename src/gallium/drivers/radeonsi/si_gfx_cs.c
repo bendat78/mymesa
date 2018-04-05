3e62fa84c7f94d93e1892b934f1d7d5eca182bb4
Marek Olšák <marek.olsak@amd.com>
2018-04-05 17:54:39 -0400

    radeonsi: make sure CP DMA is idle at the end of IBs

--- src/gallium/drivers/radeonsi/si_gfx_cs.c
+++ src/gallium/drivers/radeonsi/si_gfx_cs.c
@@ -126,7 +126,10 @@ void si_flush_gfx_cs(struct si_context *ctx, unsigned flags,
 	}
 	ctx->gfx_last_ib_is_busy = wait_flags == 0;
 
-	si_emit_cache_flush(ctx);
+	/* Make sure CP DMA is idle at the end of IBs after L2 prefetches
+	 * because the kernel doesn't wait for it. */
+	if (ctx->chip_class >= CIK)
+		si_cp_dma_wait_for_idle(ctx);
 
 	if (ctx->current_saved_cs) {
 		si_trace_emit(ctx);
