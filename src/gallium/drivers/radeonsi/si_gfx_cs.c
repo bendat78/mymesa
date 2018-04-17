494b3d56ee27a2841a20fd08fb22e60955696183
Benedikt Schemmer <ben@besd.de>
2018-04-17 20:47:45 +0200

    merge conflict

--- src/gallium/drivers/radeonsi/si_gfx_cs.c
+++ src/gallium/drivers/radeonsi/si_gfx_cs.c
@@ -214,20 +214,10 @@ void si_begin_new_gfx_cs(struct si_context *ctx)
 	 * TODO: Do we also need to invalidate CB & DB caches?
 	 */
 	ctx->flags |= SI_CONTEXT_INV_ICACHE |
-<<<<<<< HEAD
-	      SI_CONTEXT_INV_SMEM_L1 |
-		      SI_CONTEXT_INV_VMEM_L1 |
-		      SI_CONTEXT_INV_GLOBAL_L2 |
-		      SI_CONTEXT_START_PIPELINE_STATS; // |
-//		      SI_CONTEXT_FLUSH_AND_INV_DB |
-//		      SI_CONTEXT_FLUSH_AND_INV_DB_META |
-//		      SI_CONTEXT_FLUSH_AND_INV_CB;
-=======
 		      SI_CONTEXT_INV_SMEM_L1 |
 		      SI_CONTEXT_INV_VMEM_L1 |
 		      SI_CONTEXT_INV_GLOBAL_L2 |
 		      SI_CONTEXT_START_PIPELINE_STATS;
->>>>>>> 6e22ad6edc34dc07a08b41a781f1c37ff3c536e7
 
 	/* set all valid group as dirty so they get reemited on
 	 * next draw command
