3a364c5c38901c0042129d58dea84032fe933fe5
Benedikt Schemmer <ben@besd.de>
2018-05-21 20:50:30 +0200

    try linux image

--- src/util/disk_cache.c
+++ src/util/disk_cache.c
@@ -28,12 +28,12 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-//#include <sys/mman.h>
-//#include <unistd.h>
+#include <sys/mman.h>
+#include <unistd.h>
 #include <fcntl.h>
-//#include <pwd.h>
+#include <pwd.h>
 #include <errno.h>
-//#include <dirent.h>
+#include <dirent.h>
 #include "zlib.h"
 
 #include "util/crc32.h"
